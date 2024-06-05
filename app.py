900d1a07052430cdbaddad7732e8583d29373e4b
chip902 <68761150+chip902@users.noreply.github.com>
2024-06-05 01:37:22 -0400

    update to include multiple excels

--- app.py
+++ app.py
@@ -33,26 +33,18 @@ def main():
     with open('project_credentials.json', 'r', encoding='utf-8') as file:
         project_credentials = json.load(file)
 
-    all_events = {}
-
     for index, credentials in enumerate(project_credentials):
         api_key = credentials['api_key']
         secret_key = credentials['secret_key']
 
         events = get_amplitude_events(api_key, secret_key, index)
-        all_events[api_key] = events
-
-    for api_key, events in all_events.items():
-        print(f"API Key: {api_key}")
         if events:
             df = pd.DataFrame(events)
-            df.to_excel(f'events_project_{index}.xlsx', index=False)
+            df.to_excel(f'events_project_{index + 1}.xlsx', index=False)
             print(f"Exported events for project {
-                  index} to events_project_{index}.xlsx")
-            print(df)
+                  index + 1} to events_project_{index + 1}.xlsx")
         else:
-            print("No active events found.")
-        print("\n")
+            print(f"No active events found for project {index + 1}")
 
 
 if __name__ == '__main__':
